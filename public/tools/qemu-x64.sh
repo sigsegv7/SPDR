254706992ce5e5ddb10c2ee26871c22b8dc4b9ad
Chloe Moffett <chloe@mirocom.org>
2026-06-11 02:29:02 +0000

    tools: Add QEMU run wrapper script
    Signed-off-by: Chloe Moffett <chloe@mirocom.org>

---
+++ public/tools/qemu-x64.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Copyright (c) 2026, Chloe M.
+# Provided under the BSD-3 clause
+#
+# Description: QEMU x86_64 run wrapper
+# Author: 	   Chloe M.
+#
+
+qemu-system-x86_64            \
+    -cdrom artifacts/spdr.iso \
+    --enable-kvm              \
+    -m 2G                     \
+    -monitor stdio            \
+    -cpu host
