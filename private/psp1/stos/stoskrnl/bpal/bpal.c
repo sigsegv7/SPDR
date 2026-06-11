2370d614434b1c22cf5d54325a57ebe70665ae50
Chloe M. <chloe@mirocom.org>
2026-06-10 21:47:14 -0400

    stos: Add boot protocol abstraction layer (BPAL) stubs
    Signed-off-by: Chloe M. <chloe@mirocom.org>

---
+++ private/psp1/stos/stoskrnl/bpal/bpal.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026, Chloe M.
+ * Provided under the BSD-3 clause.
+ *
+ * Description: Boot Protocol Abstraction Layer
+ * Author:      Chloe M.
+ */
+
+#include <ke/bpal.h>
+
+ST_STATUS
+KeBpalInit(VOID)
+{
+    return STATUS_SUCCESS;
+}
---
+++ private/psp1/stos/stoskrnl/head/ke/bpal.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2026, Chloe M.
+ * Provided under the BSD-3 clause.
+ *
+ * Description: Boot Protocol Abstraction Layer
+ * Author:      Chloe M.
+ */
+
+#ifndef _KE_BPAL_H_
+#define _KE_BPAL_H_ 1
+
+#include <stapi/status.h>
+#include <stdef.h>
+
+#ifndef BOOT_PROTOCOL
+#define BOOT_PROTOCOL "limine"
+#endif  /* !BOOT_PROTOCOL */
+
+/*
+ * Represents the handle used to abstract away boot loader
+ * specific protocols. It acts as its own protocol for the
+ * sake of indirection.
+ *
+ * @StLoadBase: Slut technology kernel load base
+ */
+typedef struct {
+    UPTR StLoadBase;
+} KE_BPAL_HANDLE;
+
+/*
+ * Initialize the boot protocol abstraction layer
+ */
+ST_STATUS KeBpalInit(VOID);
+
+#endif  /* !_KE_BPAL_H_ */
