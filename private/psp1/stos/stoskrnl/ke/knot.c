661ce9a9a4095518b3e97ebc40ea889d77ed373d
Chloe M. <chloe@mirocom.org>
2026-06-12 18:49:01 -0400

    stos: knot: Improve kernel knot path
    Signed-off-by: Chloe M. <chloe@mirocom.org>

--- private/psp1/stos/stoskrnl/ke/knot.c
+++ private/psp1/stos/stoskrnl/ke/knot.c
@@ -6,21 +6,37 @@
  * Author:      Chloe M.
  */
 
-#include <stdef.h>
+#include <drivers/bootvid/fbio.h>
+#include <hal/prim.h>
 #include <ex/trace.h>
+#include <stdef.h>
 
 /* Globals */
 static CHAR KnotBuf[256];
 static va_list Ap;
+static BOOTCONS_ATTR KnotAttr = {
+    .Background = 0x000080,
+    .Foreground = 0xFFFFFF
+};
+
+static CHAR KnotMessage[] = {
+    "SystemPaw DR has ran into a wittle issue and the kernel pilot kitty\n"
+    "has suspended the CPU to prevent damage to your machine.\n\n"
+    "Press and hold the power button until the screen blanks.\n"
+};
 
 NO_RETURN VOID
 KiKnot(const CHAR *Fmt, ...)
 {
     va_start(Ap, Fmt);
     FmtPrintf(KnotBuf, sizeof(KnotBuf), Fmt, Ap);
+    BootVidInitCons(&KnotAttr);
 
-    TRACE("\033[31;40m*******************************************\n");
-    TRACE("ah!~ fuck, i've been knotted~ @.@\n");
+    TRACE("---- Ah!~ fuck, I've been knotted~ @.@ ----\n");
+    TRACE(KnotMessage);
     TRACE("knot: %s", KnotBuf);
-    for (;;);
+
+    for (;;) {
+        MdCpuSuspend();
+    }
 }
