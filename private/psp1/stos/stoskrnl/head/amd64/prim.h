76015bef89cb5a916701e19d66be987ac5115067
Chloe M. <chloe@mirocom.org>
2026-06-13 03:49:03 -0400

    stos/amd64: prim: Add atomic swap helper
    Signed-off-by: Chloe M. <chloe@mirocom.org>

--- private/psp1/stos/stoskrnl/head/amd64/prim.h
+++ private/psp1/stos/stoskrnl/head/amd64/prim.h
@@ -14,4 +14,30 @@
 #define MdCpuSuspend()  ASMV("hlt")
 #define MdCpuSpinWait() ASMV("pause")
 
+/*
+ * Atomic swap operation
+ *
+ * @Ptr: Location to swap with @Value
+ * @Value: Value to swap to @Ptr
+ */
+ALWAYS_INLINE static inline QUAD
+MdCpuAswap(QUAD *Ptr, QUAD Value)
+{
+    QUAD RetVal;
+
+    if (Ptr == NULL) {
+        return 0;
+    }
+
+    RetVal = *(volatile QUAD *)Ptr;
+    ASMV(
+        "xchg %0, %1\n"
+        : "+m" (*Ptr), "+r" (Value)
+        :
+        : "memory"
+    );
+
+    return RetVal;
+}
+
 #endif  /* !_MACHINE_PRIM_H_ */
