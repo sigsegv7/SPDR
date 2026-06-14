07e14abf917553456a3ff66bb25a4c1ebc0a3ff4
Chloe M. <chloe@mirocom.org>
2026-06-14 17:53:36 -0500

    public-sdk: Add initial unit defs
    Signed-off-by: Chloe M. <chloe@mirocom.org>

---
+++ public/sdk/head/units.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2026, Chloe M.
+ * Provided under the BSD-3 clause.
+ *
+ * Description: Basic global definitions and types
+ * Author:      Chloe M.
+ */
+
+#ifndef _SDK_UNITS_H_
+#define _SDK_UNITS_H_ 1
+
+/* Storage size units */
+#define UNIT_GIB 0x40000000
+#define UNIT_MIB 0x100000
+
+/* Time units */
+#define UNIT_FS_PER_MS 1000000000000ULL
+
+#endif  /* !_SDK_UNITS_H_ */
