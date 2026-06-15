9e16b85af88dac4f8708d7c537c8efb0d0b57a12
Chloe M. <chloe@mirocom.org>
2026-06-15 02:10:32 -0500

    stos/amd64: Add initial machine page defs
    Signed-off-by: Chloe M. <chloe@mirocom.org>

---
+++ private/psp1/stos/stoskrnl/head/amd64/page.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2026, Chloe M.
+ * Provided under the BSD-3 clause.
+ *
+ * Description: Machine specific page definitions
+ * Author:      Chloe M.
+ */
+
+#ifndef _MACHINE_PAGE_H_
+#define _MACHINE_PAGE_H_ 1
+
+/* Pagesize */
+#define PAGESIZE 4096
+#define LOG2_PAGESIZE 12
+
+#endif  /* !_MACHINE_PAGE_H_ */
