7f95bfc6a7d2c0b9938205eb32a6c61055b86d1e
Chloe M. <chloe@mirocom.org>
2026-06-16 16:05:41 -0500

    stos: ps: Add EPROCESS allocation
    Signed-off-by: Chloe M. <chloe@mirocom.org>

--- private/psp1/stos/stoskrnl/head/ps/ps.h
+++ private/psp1/stos/stoskrnl/head/ps/ps.h
@@ -9,10 +9,14 @@
 #ifndef _PS_PS_H_
 #define _PS_PS_H_ 1
 
+#include <stapi/status.h>
 #include <stdef.h>
 
 /* Maximum length of process name */
 #define PROCESS_NAMESZ 32
+
+/* For allocations */
+#define PS_POOL_TAG 'PS'
 
 /*
  * Represents a process
@@ -24,5 +28,14 @@ typedef struct {
     CHAR Name[PROCESS_NAMESZ];
     UQUAD ProcessId;
 } EPROCESS;
+
+/*
+ * Create a new process
+ *
+ * @Name:   Name of process to create
+ * @Flags:  Process init flags
+ * @Result: Result is written here
+ */
+ST_STATUS PsCreateProcess(const CHAR *Name, USHORT Flags, EPROCESS **Result);
 
 #endif  /* !_PS_PS_H_ */
---
+++ private/psp1/stos/stoskrnl/ps/ps.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2026, Chloe M.
+ * Provided under the BSD-3 clause.
+ *
+ * Description: High-level process management
+ * Author:      Chloe M.
+ */
+
+#include <ps/ps.h>
+#include <ex/pool.h>
+#include <ke/spinlock.h>
+#include <string.h>
+
+static USIZE NextPid = 0;
+static KE_SPINLOCK PidLock;
+
+/*
+ * Allocate a new process ID
+ */
+static USIZE
+AllocatePid(VOID)
+{
+    USIZE Pid;
+
+    KeSpinLockAcq(&PidLock, false);
+    Pid = NextPid++;
+    KeSpinLockRel(&PidLock);
+    return Pid;
+}
+
+ST_STATUS
+PsCreateProcess(const CHAR *Name, USHORT Flags, EPROCESS **Result)
+{
+    EPROCESS *Process;
+    USIZE NameLen;
+
+    if (Name == NULL) {
+        return STATUS_INVALID_PARAM;
+    }
+
+    NameLen = RtlStrLen(Name);
+    if (NameLen >= PROCESS_NAMESZ - 1) {
+        return STATUS_NAME_TOO_LONG;
+    }
+
+    Process = ExAllocatePoolWithTag(
+        NON_PAGED_POOL,
+        sizeof(*Process),
+        PS_POOL_TAG
+    );
+
+    if (Process == NULL) {
+        return STATUS_NO_MEMORY;
+    }
+
+    RtlMemSet(Process, 0, sizeof(*Process));
+    RtlMemCpy(Process->Name, Name, NameLen);
+    Process->ProcessId = AllocatePid();
+
+    *Result = Process;
+    return STATUS_SUCCESS;
+}
