d7de30b30ab5285d0e8c4dc97a4edcfdb53c39b8
Chloe M. <chloe@mirocom.org>
2026-06-16 20:04:28 -0500

    ssm: Add Sex Service Manager stub
    Signed-off-by: Chloe M. <chloe@mirocom.org>

---
+++ private/psp1/ssm/README.md
@@ -0,0 +1,4 @@
+# The Sex Service Manager
+
+The sex service manager is the first user program that is started and is responsible for preparing the system
+for operation and starting other services.
---
+++ private/psp1/ssm/server/ssm.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026, Chloe M.
+ * Provided under the BSD-3 clause.
+ *
+ * Description: The Sex Service Manager server
+ * Author:      Chloe M.
+ */
+
+#include <stdef.h>
+
+ULONG
+Main(VOID)
+{
+    return 0;
+}
